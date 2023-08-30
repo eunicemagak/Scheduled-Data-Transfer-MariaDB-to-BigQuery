00978f51c74ee541a531a3dd6cfe7a8f45f9018e
eunice <eunice.magak@emalify.com>
2023-08-30 16:58:16 +0300

    adding inline comments

--- dlrs.py
+++ dlrs.py
@@ -3,18 +3,28 @@ from google.cloud import bigquery
 from my_logging import logger
 from utils import connect_to_mariadb, initialize_bigquery_client, create_bigquery_dataset, retrieve_bigquery_table_schema
 
-# BigQuery client and dataset configuration
-project_id = 'rt-warehouse'
-dataset_id = 'EmalifyMariaDB'
-table_id = 'dlrs'
-service_account_key_file = '.credentials'
+# Configuration for BigQuery and Google Cloud Storage
+project_id = 'rt-warehouse'          # The ID of the Google Cloud project
+dataset_id = 'EmalifyMariaDB'               # The ID of the BigQuery dataset
+table_id = 'dlrs'                  # The ID of the BigQuery table
+service_account_key_file = '.credentials'     # Path to the service account key file
 
-
-# Connect to MariaDB database
+# Establish a connection to MariaDB
 mariadb_conn = connect_to_mariadb()
 
-    
+
+""" Function to fetch schema for a given table from MariaDB """
 def fetch_table_schema_from_mariadb(mariadb_conn, table_name):
+    """
+    Fetches the schema of a specified table from MariaDB.
+    
+    Args:
+        mariadb_conn (connection): A connection to the MariaDB database.
+        table_name (str): The name of the table.
+        
+    Returns:
+        list: A list of tuples representing the table schema.
+    """
     logger.info(f'Fetching schema for {table_name}')
     mariadb_cursor = mariadb_conn.cursor()
     table_schema_query = f"DESCRIBE {table_name}"
@@ -23,8 +33,18 @@ def fetch_table_schema_from_mariadb(mariadb_conn, table_name):
     logger.info(f'Schema for {table_name} is {table_schema}')
     return table_schema
 
-
+    
+# Function to convert MySQL schema to BigQuery schema
 def convert_mysql_schema_to_bigquery_schema(table_schema):
+    """
+    Converts a MySQL table schema to a BigQuery-compatible schema.
+    
+    Args:
+        table_schema (list): A list of tuples representing the MySQL table schema.
+        
+    Returns:
+        list: A list of `bigquery.SchemaField` objects representing the BigQuery schema.
+    """
     logger.info(f'Converting {table_schema} to bigquery')
     bq_schema = []
     for column in table_schema:
@@ -36,7 +56,18 @@ def convert_mysql_schema_to_bigquery_schema(table_schema):
     return bq_schema
 
 
+ # Function to retrieve the last ID from a BigQuery table
 def get_bq_last_id(bigquery_client, table):
+    """
+    Retrieves the last ID from a specified BigQuery table.
+    
+    Args:
+        bigquery_client (bigquery.Client): An initialized BigQuery client.
+        table (bigquery.Table): The BigQuery table to retrieve the last ID from.
+        
+    Returns:
+        int: The last ID value.
+    """
     logger.info(f'Getting big query last id for {table}')
     # Retrieve the maximum ID from the BigQuery table
     max_id_query = f"SELECT MAX(id) FROM {table.dataset_id}.{table.table_id}"
@@ -44,12 +75,23 @@ def get_bq_last_id(bigquery_client, table):
 
     for row in max_id_result:
         last_id = row[0]
-    #last_id = max_id_result[0][0]
     logger.info(f'Big Query last id is {last_id}')
     return last_id
 
 
+# Function to fetch data from MariaDB for a given table
 def fetch_data_from_mariadb(mariadb_cursor, table_name, last_id=None):
+    """
+    Fetches data from MariaDB for a specified table.
+    
+    Args:
+        mariadb_cursor (cursor): A MariaDB cursor object.
+        table_name (str): The name of the table.
+        last_id (int, optional): The last ID value. Defaults to None.
+        
+    Returns:
+        list: A list of rows retrieved from MariaDB.
+    """
     logger.info(f'Fetching data from mariadb for {table_name}')
     if last_id is None:
         # Fetch all data from the MariaDB table
@@ -58,7 +100,7 @@ def fetch_data_from_mariadb(mariadb_cursor, table_name, last_id=None):
         # Fetch data from MySQL table with ID greater than the last ID in BigQuery
         mysql_query = f"SELECT * FROM {table_name} WHERE id > {last_id} LIMIT 10000"
 
-    logger.info(f'MySQL Query: {mysql_query}')
+    logger.info(f'MySQL Query: {mysql_query}') 
 
     mariadb_cursor.execute(mysql_query)
     rows = mariadb_cursor.fetchall()
@@ -67,7 +109,15 @@ def fetch_data_from_mariadb(mariadb_cursor, table_name, last_id=None):
 
 
 def insert_data_into_bigquery(bigquery_client, table, rows):
-    logger.info(f'Inserting {rows} into {table} ')
+    """
+    Inserts data into a specified BigQuery table.
+    
+    Args:
+        bigquery_client (bigquery.Client): An initialized BigQuery client.
+        table (bigquery.Table): The BigQuery table to insert data into.
+        rows (list): A list of rows to insert.
+    """
+    logger.info(f'Inserting {len(rows)} rows into {table} ')  # Updated log message
     batch_size = 1000
     for i in range(0, len(rows), batch_size):
         batch_rows = rows[i:i + batch_size]
@@ -76,12 +126,16 @@ def insert_data_into_bigquery(bigquery_client, table, rows):
         errors = bigquery_client.insert_rows(table, batch_rows, selected_fields=table.schema)
 
         if errors:
-            print(f"Failed to insert rows into BigQuery table: {errors}")
+            logger.error(f"Failed to insert rows into BigQuery table: {errors}")  # Updated log message
         else:
-            print("Batch insertion into BigQuery table completed successfully!")
+            logger.info("Batch insertion into BigQuery table completed successfully!")  # Updated log message
 
 
-def main ():
+
+def main():
+    """
+    Main function for orchestrating the data migration process.
+    """
     # Initialize BigQuery client
     bigquery_client = initialize_bigquery_client(project_id, service_account_key_file)
 
@@ -110,6 +164,8 @@ def main ():
     # Close connections
     mariadb_conn.close()
 
+
+"""ensures that the main function is run in an infinite loop at regular intervals (every 10 seconds) """
 if __name__ == "__main__":
     while True:
         main()
--- utils.py
+++ utils.py
@@ -2,35 +2,60 @@ import mysql.connector
 from google.cloud import bigquery
 from google.oauth2 import service_account
 from dotenv import load_dotenv
+from my_logging import logger
 import os
 
+# Load environment variables from .env file
 load_dotenv()
 
+# Connect to the MariaDB database
 def connect_to_mariadb():
+    logger.info("Connecting to MariaDB...")
+    # Establish a connection to MariaDB using environment variables
     mariadb_conn = mysql.connector.connect(
-        host=os.getenv('DB_HOST'),
-        port=os.getenv('DB_PORT'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        database=os.getenv('DB_DATABASE'))
+        host=os.getenv('DB_HOST'),         # Database host from environment
+        port=os.getenv('DB_PORT'),         # Database port from environment
+        user=os.getenv('DB_USER'),         # Database user from environment
+        password=os.getenv('DB_PASSWORD'), # Database password from environment
+        database=os.getenv('DB_DATABASE')  # Database name from environment
+    )
+    logger.info("Connected to MariaDB.")
     return mariadb_conn
 
 
+# Initialize the BigQuery client using a service account key
 def initialize_bigquery_client(project_id, service_account_key_file):
+    logger.info("Initializing BigQuery client...")
+    # Load credentials from the provided service account key file
     credentials = service_account.Credentials.from_service_account_file(service_account_key_file)
+    
+    # Create a BigQuery client with the specified project ID and credentials
     bigquery_client = bigquery.Client(project=project_id, credentials=credentials)
+    logger.info("BigQuery client initialized.")
     return bigquery_client
 
 
+# Create a BigQuery dataset if it doesn't exist
 def create_bigquery_dataset(bigquery_client, dataset_id):
+    logger.info("Creating BigQuery dataset...")
+    # Define a reference to the dataset
     dataset_ref = bigquery_client.dataset(dataset_id)
+    
+    # Create a BigQuery dataset with the specified ID and allow existence check
     dataset = bigquery.Dataset(dataset_ref)
     dataset = bigquery_client.create_dataset(dataset, exists_ok=True)
+    logger.info("BigQuery dataset created.")
     return dataset
 
 
+# Retrieve the schema of a specific BigQuery table
 def retrieve_bigquery_table_schema(bigquery_client, dataset_id, table_id):
+    logger.info("Retrieving BigQuery table schema...")
+    # Define references to the dataset and the table within it
     dataset_ref = bigquery_client.dataset(dataset_id)
     table_ref = dataset_ref.table(table_id)
+    
+    # Get the schema of the specified BigQuery table
     table = bigquery_client.get_table(table_ref)
+    logger.info("BigQuery table schema retrieved.")
     return table
